d803ea5ed5fd1187a48e13b1795b15ac8b81cd9b
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-08-27 16:24:25 +0000

    Page Downloader Activity

---
+++ fetcher.js
@@ -0,0 +1,21 @@
+'use-strict';
+//Required modules - request and fs
+const request = require('request');
+const fileSystem = require('fs');
+
+//Get the inputs passed as CLI argument
+const cliArg = process.argv.slice(2);
+
+const httpRequest = cliArg[0];
+const fileToWrite = cliArg[1];
+
+//Request function by passing the http path(input from CLI)
+request(httpRequest, (error, response, body) => {
+  //Write the html body as index.html file asynchronously
+  fileSystem.writeFile(fileToWrite, body, (err) => {
+    if (err) throw err;
+    fileSystem.stat(fileToWrite, (err, stat) => {
+      console.log(`Downloaded and saved ${stat.size} bytes to ${fileToWrite}`);
+    });
+  });
+});
